cf7ff0d8ce5df47317da482602d5f842b4ca013f
Bajasahay <adven.abhisahay@gmail.com>
2021-01-24 10:56:06 +0530

    BubbleSort

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+public class BubbleSort
+{
+    public static void main(String args[])
+    {
+        int[] arr = {8,7,6,5,4,3,2,1};
+        for(int i = 0; i<arr.length;i++)
+        {
+            for(int j = 0; j<arr.length-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    swap(j,arr);
+                }
+            }
+        }
+
+        for(int k = 0; k<arr.length;k++)
+        {
+            System.out.println(arr[k]);
+        }
+    }
+
+    private static void swap(int j, int[] arr)
+    {
+        int c = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = c;
+    }
+}
