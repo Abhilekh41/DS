940cc50c647410ef83cf3feec84e3fae3b33a591
Bajasahay <adven.abhisahay@gmail.com>
2021-02-12 19:01:50 +0530

    updated with Binary Search

--- src/SearchingAndSorting/BinarySearch.java
+++ src/SearchingAndSorting/BinarySearch.java
@@ -4,21 +4,21 @@ public class BinarySearch
 {
     public static void main(String args[])
     {
-        int[] arr = {1,2,3,4,5,6,7,8};
-        System.out.println(binarySearch(arr,0,arr.length-1,1));
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(binarySearch(arr,0,arr.length-1,7));
     }
 
     public static int binarySearch(int[] arr,int startIndex, int endIndex, int element)
     {
         if(endIndex>=startIndex)
         {
-            int mid = (startIndex+endIndex-1)/2;
+            int mid = (startIndex+endIndex)/2;
 
             if(arr[mid]==element)
             {
                 return mid;
             }
-            if(mid>element)
+            if(arr[mid]>element)
             {
                 return binarySearch(arr,startIndex,mid-1,element);
             }
